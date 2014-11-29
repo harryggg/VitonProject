076d5c5a57ea0995e4133b8fb6a09b4e01360d7e
Ma Tanghao <ma.tanghao@dhs.sg>
2014-11-29 23:22:21 +0800

    updatetime change
    5min to 30min

--- mobile/src/main/java/com/example/fengling/vitontest/Flags.java
+++ mobile/src/main/java/com/example/fengling/vitontest/Flags.java
@@ -4,7 +4,7 @@ package com.example.fengling.vitontest;
  * Created by harry on 2014/11/12.
  */
 public class Flags {
-    public static final int UPDATEINTERVAL = 5*60*1000; //
+    public static final int UPDATEINTERVAL = 30*60*1000; //
     public static final String START_ACTIVITY_PATH_START = "/viton/start";
     public static final String START_ACTIVITY_PATH_STOP = "/viton/stop";
     public static final String START_ACTIVITY_PATH_GETDATA = "/viton/getData";
